ef3c3e6f5ccee41b212529125aa7d5073181e9b3
CharlieGao <chutian-gao@uiowa.edu>
2013-03-27 17:13:43 -0500

    B

--- src/editDistance.java
+++ src/editDistance.java
@@ -50,7 +50,7 @@ public class editDistance {
 	
 	System.out.println(lenT);
 	System.out.println("Pattern: "+strP);
-	System.out.println("Target: ");		
+	System.out.println("Target: "+B);		
 	/**********************************************************************************/
 	bestCur[0]=strP;
 	int i,j;
